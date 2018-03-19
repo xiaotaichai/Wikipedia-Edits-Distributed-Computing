6da5ad6d15ef0a0b4c96c96c50013f32b7a2f28a
fbchow <fannybchow@gmail.com>
2018-03-19 15:02:49 -0400

    committing because I've got trouble copy and pasting from vim

--- unique_article.py
+++ unique_article.py
@@ -3,11 +3,15 @@ counter = 0
 my_list=[0]*200000000
 with gzip.open("/Akamai_scratch/enwiki-20080103.good.gz",'r') as file:
 	for line in file:
-	    while counter < 100:	
+	    if counter < 10:	
                 revision = line.decode().split("\x1f")
 		my_list[counter] = revision[0]
 		counter +=1
         my_list = my_list[0:counter]
 	my_list = list(set(my_list))
-	file.write(my_list)
-	print (len(my_list))
+	#file.write(my_list)
+	#print (len(my_list))
+
+        newfile = gzip.open('newfilename.gz', 'wb')
+        newfile.write(lines)
+        newfile.close()
