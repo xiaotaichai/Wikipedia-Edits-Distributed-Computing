c978572691b0b0b7ca2d6db304de9d4c5a39d9e9
fbchow <fannybchow@gmail.com>
2018-03-19 10:29:30 -0400

    put constraint on counter at 100 for testing purposes

--- unique_article.py
+++ unique_article.py
@@ -3,7 +3,8 @@ counter = 0
 list=[0]*200000000
 with gzip.open("/Akamai_scratch/enwiki-20080103.good.gz",'r') as file:
 	for line in file:
-		revision = line.decode().split("\x1f")
+	    while counter < 100:	
+                revision = line.decode().split("\x1f")
 		list[counter] = revision[0]
 		counter +=1
 	list = list[0,counter]
