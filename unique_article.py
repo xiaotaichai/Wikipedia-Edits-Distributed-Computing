50ad1860b3f5a129bfa30bda0dc3b2a45d5026a2
Xiaotai <xiaotai.chai17@ncf.edu>
2018-03-18 22:47:44 -0400

    create a list with unique article id

---
+++ unique_article.py
@@ -0,0 +1,12 @@
+import gzip
+counter = 0
+list=[0]*200000000
+with gzip.open("enwiki-20080103.good.gz",'r') as file:
+	for line in file:
+		revision = line.decode().split("\x1f")
+		list[counter] = revision[0]
+		counter +=1
+	list = list[0,counter]
+	list = unique(list)
+	list.write('unique_article\n')
+	print (len(list))
