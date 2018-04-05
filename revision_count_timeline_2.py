5f5197e36070e1acd81dc558dea29be3bbe20f70
Lydia LaSeur <lydialaseur@gmail.com>
2018-04-05 19:53:22 -0400

    now grabs username and userid for author of each revision, gets all articles with at least 5 years of history

--- revision_count_timeline_2.py
+++ revision_count_timeline_2.py
@@ -15,11 +15,12 @@ class RevisionTimeline(MRJob):
         article_name = article_info[3]
 
         revision_datetime_str = article_info[4]
-
+        user_name = article_info[5]
+        user_id = article_info[6]
         revision_length = int(record[12].split(' ')[1])
         minor_flag = int(record[11].split(' ')[1])
 
-        yield [article_id, article_name], [revision_datetime_str, revision_length, minor_flag]
+        yield [article_id, article_name], [revision_datetime_str, revision_length, minor_flag, user_name, user_name]
 
 
     def reducerCreateTimeline(self, key, revisions):
@@ -34,21 +35,21 @@ class RevisionTimeline(MRJob):
 
         # check if creation was before 01-01-03, so each article has at least a five year history
 
-        # if creation_datetime < dt.datetime.strptime('2003-01-01','%Y-%m-%d'):
-        num_revisions = len(revisions)
-        normalized_revision_timeline = [[] for i in range(num_revisions)]
+        if creation_datetime < dt.datetime.strptime('2003-01-01','%Y-%m-%d'):
+            num_revisions = len(revisions)
+            normalized_revision_timeline = [[] for i in range(num_revisions)]
 
-        # creation_datetime = dt.datetime.strptime(values[1],'%Y-%m-%dT%H:%M:%SZ')
-        # revisions = values[0]
-        i = 0
-        for r in revisions:
-            revision_datetime = dt.datetime.strptime(r[0],'%Y-%m-%dT%H:%M:%SZ')
-            time_since_creation = revision_datetime - creation_datetime
-            normalized_revision_timeline[i] = [time_since_creation.days, time_since_creation.seconds, r[1], r[2]]
-            i += 1
+            # creation_datetime = dt.datetime.strptime(values[1],'%Y-%m-%dT%H:%M:%SZ')
+            # revisions = values[0]
+            i = 0
+            for r in revisions:
+                revision_datetime = dt.datetime.strptime(r[0],'%Y-%m-%dT%H:%M:%SZ')
+                time_since_creation = revision_datetime - creation_datetime
+                normalized_revision_timeline[i] = [time_since_creation.days, time_since_creation.seconds, r[1], r[2],r[3],r[4]]
+                i += 1
 
 
-        yield key , [creation_datetime.strftime('%Y-%m-%d %H:%M:%S'), num_revisions, normalized_revision_timeline]
+            yield key , [creation_datetime.strftime('%Y-%m-%d %H:%M:%S'), num_revisions, normalized_revision_timeline]
         # yield key , [revisions, creation_datetime_str, num_revisions]
 
     # def reducerCreateTimeline(self, key, values):
