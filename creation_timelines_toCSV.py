52eb0b5616d93134e07eefc94d4e6e11df765ac4
Lydia LaSeur <lydialaseur@gmail.com>
2018-04-06 22:46:23 -0400

    adding script to convert data to an easier format

---
+++ creation_timelines_toCSV.py
@@ -0,0 +1,14 @@
+import os
+import re
+outfile = open('./creation_timelines_5yrs_new.csv', 'w')
+# write header
+outfile.write('article_id,article_name,creation_datetime,num_revisions,all_revisions')
+
+line_pattern = '\["([0-9]+?)", "(.+?)"\]\s*\["([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})", ([0-9]+?), \[(.+)\]\]'
+with open('./creation_timelines_all.txt','r') as infile:
+    for line in infile:
+        parts = re.findall(line_pattern, line)
+        new_line = '{0},"{1}",{2},{3},"{4}"\n'.format(parts[0],parts[1],parts[2],parts[3],parts[4])
+        outfile.write(new_line)
+
+outfile.close()
