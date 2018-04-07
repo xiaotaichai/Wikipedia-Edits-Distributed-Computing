ffe367842f44d03123c328d2881d6ab98949a95a
Lydia LaSeur <lydialaseur@gmail.com>
2018-04-06 22:52:15 -0400

    whoops, mixed up input and output filenames

--- creation_timelines_toCSV.py
+++ creation_timelines_toCSV.py
@@ -2,12 +2,12 @@ import os
 import re
 
 progress = 0
-outfile = open('./creation_timelines_5yrs_new.csv', 'w')
+outfile = open('./creation_timelines_5yrs.csv', 'w')
 # write header
 outfile.write('article_id,article_name,creation_datetime,num_revisions,all_revisions')
 
 line_pattern = '\["([0-9]+?)", "(.+?)"\]\s*\["([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})", ([0-9]+?), \[(.+)\]\]'
-with open('./creation_timelines_5yrs.txt','r') as infile:
+with open('./creation_timelines_5yrs_new.txt','r') as infile:
     for line in infile:
         parts = re.findall(line_pattern, line)
         new_line = '{0},"{1}",{2},{3},"{4}"\n'.format(parts[0],parts[1],parts[2],parts[3],parts[4])
