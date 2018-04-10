7a39c4e6d16d33e5fb7aa92bda5543f838dc18da
fbchow <fannybchow@gmail.com>
2018-04-09 23:33:40 -0400

    cleaned up some general documentation

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Wikipedia-Edits-Distributed-Computing
 
+ 
+# Questions of Interest
+*  What are the most commonly edited Wikipedia articles?
+* What trends & behaviours can we discern from article revision history?
+* Do popular revisions coincide with current events, history, or pop culture?   
 
+# Data Set
+SNAP (Stanford) compressed zip file with all of Wikipedia's revisions from its inception in 2001 to January 2008.  
+
+# Initial Sampling Worfklow  
 * Generate a list of unique article_ids  
 * Aggregate attributes associated with each article_id  
 
+# Development  
 
-### Remember   
+### Run   
 
 Run `python3 unique_article.py`  
 Otherwise the base python2.7 will scream at you.
 
-### How to read head  
+### How to read head of gz file  
 ```
 $ gzip -cd enwiki-20080103.good.gz | head -n 1
 $ gzip -cd unique_all_articleids_3.gz | head -n 10
--- unique_article.py
+++ unique_article.py
@@ -1,49 +1,7 @@
-#import gzip
-#counter = 0
-#my_list=[0]*200000000
-#with gzip.open("/Akamai_scratch/enwiki-20080103.good.gz",'r') as file:
-#        for line in file:
-#                if counter <= 1:
-#			revision = line.decode().split("\x1f")
-#			print("after decode", revision)
-#                        my_list[counter] = revision[0]
-#                        counter +=1
-#                else:
-#                    break
-#        my_list = my_list[0:counter]
-#        my_list = list(set(my_list))
-#        # file.write(my_list)
-#        print (len(my_list))
-#
-#        newfile = gzip.open('unique_all_articleids.gz', 'wt')
-#        newfile.writelines(my_list)
-#        newfile.close()
-#
-
-#import gzip
-#counter = 0
-#my_list=[0]*200000000
-#with gzip.open("/Akamai_scratch/enwiki-20080103.good.gz",'r') as file:
-#        for line in file:
-#                if counter <= 1:
-#                    print("!!!!!before revision", line)
-#                    revision = line.decode().split("\x1e")
-#                    print("!!!!!after decode", revision)
-#		    revision = revision.split(' ')
-#		    print('!!!REVISION SPLIT BY SPACES')  
-#		    my_list[counter] = revision[0]
-#                    counter +=1
-#                else:
-#                    break
-#        my_list = my_list[0:counter]
-#        my_list = list(set(my_list))
-#        # file.write(my_list)
-#        print (len(my_list))
-#
-#        newfile = gzip.open('unique_all_articleids.gz', 'wt')
-#        newfile.writelines(my_list)
-#        newfile.close()
-
+'''
+randomly subsample 1000 unique article ids
+many print statements
+'''
 
 import gzip
 counter = 0
